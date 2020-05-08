6c55d4ae77ed7439c6008dd8ae0acf49e5bbd027
Administrator <rajarsi3997@gmail.com>
2020-05-08 20:46:20 +0530

    download and clean data

---
+++ down_clean.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#downloading data
+curl "https://query1.finance.yahoo.com/v7/finance/download/GOOG?period1=1557322330&period2=1588944730&interval=1d&events=history" --output GOOG.csv
+
+#scrapping off first line in csv
+flag="0"
+while IFS=, read -r Date Open High Low Close AdjClose Volume
+do
+	if [ $flag -eq "0" ]
+	then
+		flag="1"
+		continue
+	fi
+	echo "$Date,$Open,$High,$Low,$Close,$AdjClose,$Volume" >> GOOG_CLEAN.csv
+done < GOOG.csv
